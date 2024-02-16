9931778fbad0bf333a8441738db29c05a85975fe
ikalco <73481042+ikalco@users.noreply.github.com>
2024-02-16 17:11:48 -0600

    started abstracting away sdl boiler plate, trying to get quit event to work

--- src/main.c
+++ src/main.c
@@ -1,19 +1,19 @@
+#include <SDL2/SDL_timer.h>
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
-int main(int argc, char* argv[]) {
+SDL_Window *window;
+SDL_Renderer *renderer;
 
-	// create window data type
-	SDL_Window *window;
-
+void initSDL() {
 	// init sdl2
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		printf("SDL couldn't be initialized.\n");
-		return 1;
+		exit(1);
 	} else {
 		printf("SDL video system is ready to go.\n");
 	}
-	
+
 	// create window
 	window = SDL_CreateWindow(
 		"An SDL2 window",
@@ -27,24 +27,51 @@ int main(int argc, char* argv[]) {
 	// if window is undefined print error and exit
 	if (window == NULL) {
 		printf("Couldn't create window: %s\n", SDL_GetError());
-		return 1;
+		exit(1);
 	}
 
 	// create renderer
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+}
 
-	// clear screen and draw it
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-
-	// wait 3 sec so you can see the window
-	SDL_Delay(3000);
-	
+void cleanup() {
 	// destroy the window
 	SDL_DestroyWindow(window);
 
 	// clean up
 	SDL_Quit();
+}
+
+void handle_input() {
+	SDL_Event event;
+
+	while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+			case SDL_QUIT:
+				exit(0);
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+int main(int argc, char* argv[]) {
+	initSDL();
+	atexit(cleanup);
+
+	while (1) {
+		// clear screen
+		SDL_RenderClear(renderer);
+
+		// draw a line
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_RenderDrawLine(renderer, 0, 0, 640, 480);
+
+		// draw to screen and wait 16ms (62.5 fps)
+		SDL_RenderPresent(renderer);
+		SDL_Delay(16);
+	}
 
 	return 0;
 }
