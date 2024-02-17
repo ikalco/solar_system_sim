1a93b32085d4503b2b9503c4bad64de60d14fe0b
ikalco <73481042+ikalco@users.noreply.github.com>
2024-02-16 23:38:32 -0600

    simple linked list, will be used for holding all physical objects

---
+++ src/linked_list.c
@@ -0,0 +1,49 @@
+#include "linked_list.h"
+
+List* create_list() {
+	List* new_list = (List*)malloc(sizeof(List));
+	new_list->size = 0;
+	new_list->first = NULL;
+	return new_list;
+}
+
+void add_list(List* list, void* data) {
+	Node* new_node = (Node*)malloc(sizeof(Node));
+	new_node->data = data;
+	new_node->next = list->first;
+	list->first = new_node;
+	list->size++;
+}
+
+void* remove_list(List* list) {
+	if (list->size == 0) {
+		return NULL;
+	}
+
+	Node* removed = list->first;
+	void* data = removed->data;
+	
+	list->first = removed->next;
+	list->size--;
+
+	free(removed);
+
+	return data;
+}
+
+void free_list(List* list) {
+	Node* current = list->first;
+	while(current != NULL) {
+		Node* next = current->next;
+		
+		if (current->data != NULL) {
+			free(current->data);
+		}
+		
+		free(current);
+
+		current = next;
+	}
+
+	free(list);
+}
---
+++ src/linked_list.h
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+typedef struct node {
+	void* data;
+	struct node* next;
+} Node;
+
+typedef struct list {
+	Node* first;
+	int size;
+} List;
+
+List* create_list();
+void add_list(List* list, void* data);
+void* remove_list(List* list);
+void free_list(List* list);
