51066340eefaa20495068d0adc978d5b0e2a6eaf
ikalco <73481042+ikalco@users.noreply.github.com>
2024-02-17 00:16:08 -0600

    defined some units

--- src/defines.h
+++ src/defines.h
@@ -5,7 +5,20 @@
 #define WINDOW_HEIGHT 720
 #define WINDOW_FPS 60
 
+// constants
 #define G_CONSTANT 6.67430e-11
+
+// units
+#define M 1						// meters
+#define KM 1000					// kilometers	
+#define AU 149597870700			// astronomical unit (in meters)
+#define G 0.001					// grams
+#define KG 1					// kilograms
+#define S 1						// seconds
+
+// derived units
+#define M_S 1					// meters per second
+#define KM_S 1000				// kilometers per second
 
 typedef struct vd {
 	double x;
