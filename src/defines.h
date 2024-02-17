37bf70aefd1f11aae005315b8719602ded31bdcb
ikalco <73481042+ikalco@users.noreply.github.com>
2024-02-16 23:12:44 -0600

    added gravitational constant and struct for a physical body

--- src/defines.h
+++ src/defines.h
@@ -5,6 +5,15 @@
 #define WINDOW_HEIGHT 720
 #define WINDOW_FPS 60
 
+#define G_CONSTANT 6.67430e-11
+
+typedef struct {
+	double x;
+	double y;
+	double mass;
+	double radius;
+} PhysicalBody;
+
 void initSDL();
 void cleanup();
 void handle_input();
