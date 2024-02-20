66443e331c5fa87d0d771cfd1f3150ed175b0bcb
ikalco <73481042+ikalco@users.noreply.github.com>
2024-02-20 16:47:38 -0600

    added first menu text, also adding to a menu vertical list, and defines for min/max for size

--- src/menu.c
+++ src/menu.c
@@ -12,9 +12,13 @@ MenuRoot (MenuList)
 */
 
 MenuRoot* init_menu() {
-	MenuVerticalList* list = create_menu_vlist((Color){40, 40, 40, 255}, (VectorD){20, 20}, 10);
+	MenuVerticalList* root_list = create_menu_vlist((Color){40, 40, 40, 255}, (VectorD){20, 20}, 10);
 
-	MenuNode* root_node = create_menu_node((VectorD){10, 10}, (VectorD){260, 460}, MENU_LIST, list);
+	MenuText* text1 = create_menu_text((Color){235, 235, 235, 255}, TEXT_CENTER, "Main Menu");
+	MenuNode* text1_node = create_menu_node((VectorD){0, 0}, (VectorD){MENU_MAX_SIZE, 30}, MENU_TEXT, text1);
+	add_menu_vlist(root_list, text1_node);
+
+	MenuNode* root_node = create_menu_node((VectorD){10, 10}, (VectorD){260, 460}, MENU_LIST, root_list);
 	MenuRoot* root = create_menu_root((VectorD){100, 100}, (VectorD){300, 500}, "arial.ttf", root_node);
 
 	return root;
@@ -56,4 +60,20 @@ MenuVerticalList* create_menu_vlist(Color bg_color, VectorD padding, double spac
 	list->size = 0;
 
 	return list;
+}
+
+void add_menu_vlist(MenuVerticalList* list, MenuNode* node) {
+	node->next = list->child;
+	list->child = node;
+	list->size++;
+}
+
+MenuText* create_menu_text(Color text_color, MenuTextAlign align, char* text) {
+	MenuText* menu_text = malloc(sizeof(MenuText));
+
+	menu_text->text_color = text_color;
+	menu_text->text = text;
+	menu_text->align = align;
+
+	return menu_text;
 }
--- src/menu.h
+++ src/menu.h
@@ -5,6 +5,9 @@
 #include <SDL2/SDL_ttf.h>
 #include "structs.h"
 
+#define MENU_MAX_SIZE -1
+#define MENU_MIN_SIZE -2
+
 typedef enum {
 	MENU_NONE,
 	MENU_ROOT,
@@ -13,6 +16,12 @@ typedef enum {
 	MENU_BUTTON,
 	MENU_SPACER
 } MenuType;
+
+typedef enum {
+	TEXT_LEFT,
+	TEXT_CENTER,
+	TEXT_RIGHT
+} MenuTextAlign;
 
 typedef struct menunode {
 	VectorD offset;
@@ -44,6 +53,7 @@ typedef struct {
 
 typedef struct {
 	Color text_color;
+	MenuTextAlign align;
 	char* text;
 } MenuText;
 
@@ -63,5 +73,8 @@ MenuRoot* create_menu_root(VectorD position, VectorD size, char* font, MenuNode*
 MenuNode* create_menu_node(VectorD offset, VectorD size, MenuType type, void* node);
 
 MenuVerticalList* create_menu_vlist(Color bg_color, VectorD padding, double spacing);
+void add_menu_vlist(MenuVerticalList* list, MenuNode* node);
+
+MenuText* create_menu_text(Color text_color, MenuTextAlign align, char* text);
 
 #endif
