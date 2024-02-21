c2dd4abb3ab2b52bf4a4d199409533967a03c69b
ikalco <73481042+ikalco@users.noreply.github.com>
2024-02-21 16:01:25 -0600

    coded getting offset from root for menu node's and also text alignment

--- src/menu_render.c
+++ src/menu_render.c
@@ -5,14 +5,37 @@ void render_menu_vlist(MenuRoot* root, MenuNode* list);
 void render_menu_text(MenuRoot* root, MenuNode* text);
 void render_menu_button(MenuRoot* root, MenuNode* button);
 
-SDL_Rect get_menu_offset(MenuNode *node) {
-	// TODO
-	return (SDL_Rect){0, 0, 0, 0};
+SDL_Rect get_menu_offset(MenuRoot* root, MenuNode* node) {
+	// we want to add our own offset so that's why we don't set parent here
+	MenuNode* parent = node;
+
+	SDL_Rect offset = {root->position.x, root->position.y, node->size.x, node->size.y};
+
+	while (parent != NULL) {
+		offset.x += parent->offset.x;
+		offset.y += parent->offset.y;
+		parent = parent->parent;
+	}
+
+	return offset;
 }
 
-SDL_Rect get_menu_align_offset(MenuNode *node, int text_width, int text_height) {
-	// TODO
-	return (SDL_Rect){0, 0, 0, 0};
+SDL_Rect add_menu_align_offset(SDL_Rect offset, MenuTextAlign align, int text_width, int text_height) {
+	// all alignments are vertically centered, the align is actually for the horizontal
+	offset.y += (offset.h / 2) - (text_height / 2);
+
+	switch (align) {
+		case TEXT_LEFT:
+			break;
+		case TEXT_CENTER:
+			offset.x += (offset.w / 2) - (text_width / 2);
+			break;
+		case TEXT_RIGHT:
+			offset.x += offset.w - (text_width / 2);
+			break;
+	}
+
+	return offset;
 }
 
 void render_menu_root(MenuRoot* root) {
@@ -48,11 +71,11 @@ void render_menu_text(MenuRoot* root, MenuNode* text_node) {
 
 	SDL_Texture* texture = SDL_CreateTextureFromSurface(root->menu_renderer, rendered_text);
 
-	int w, h;
-	TTF_SizeUTF8(root->font, text->text, &w, &h);
+	int text_width, text_height;
+	TTF_SizeUTF8(root->font, text->text, &text_width, &text_height);
 
-	SDL_Rect dstrect = get_menu_offset(text_node);
-	dstrect = get_menu_align_offset(text_node, w, h);
+	SDL_Rect dstrect = get_menu_offset(root, text_node);
+	dstrect = add_menu_align_offset(dstrect, text->align, text_width, text_height);
 
 	SDL_RenderCopy(
 		root->menu_renderer,
