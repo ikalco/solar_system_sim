7c2b75f0c1eafd507cdde17c2fccc683d53e4712
ikalco <73481042+ikalco@users.noreply.github.com>
2024-02-29 18:24:59 -0600

    implemented taking input for actually loading save file, seems to be an issue with sprintf making the file name string though...

--- src/scenes/load_menu/scene.c
+++ src/scenes/load_menu/scene.c
@@ -1,6 +1,16 @@
 #include "scene.h"
 #include "menu.h"
+
+#include "engine/save.h"
 #include "menu/menu.h"
+#include "menu/menu_util.h"
+#include "options.h"
+
+Scene *create_save_file_scene(char *save_file_name) {
+	// TODO
+	printf("%s\n", save_file_name);
+	return NULL;
+}
 
 void init_load_menu(SceneManager *manager, Scene *scene, SDL_Window *window) {
 	Data *data = malloc(sizeof(Data));
@@ -24,7 +34,47 @@ void cleanup_load_menu(void *data) {
 }
 
 void handle_input_load_menu(void *data, SDL_Event *event) {
-	// TODO
+	Data *menu_data = data;
+
+	if (event->type == SDL_MOUSEBUTTONDOWN) {
+		menu_data->clicked_id = find_mouse_menu_root(
+			menu_data->root, event->button.x, event->button.y
+		);
+	} else if (event->type == SDL_MOUSEBUTTONUP) {
+		int new_clicked_id = find_mouse_menu_root(
+			menu_data->root, event->button.x, event->button.y
+		);
+
+		// if we clicked something but then dragged mouse off and let go
+		// somewhere else, then just don't do anything
+		if (menu_data->clicked_id != new_clicked_id) return;
+
+		// we clicked on a save file button
+		if (menu_data->clicked_id >= SAVE_BUTTONS) {
+			MenuNode *save_file_button_node =
+				find_menu_node_id(menu_data->root->root, menu_data->clicked_id);
+			MenuButton *save_file_button = save_file_button_node->node;
+			char save_file_name[512];
+
+			sprintf(
+				save_file_name,
+				"%s/%s.save",
+				DEFAULT_SAVE_PATH,
+				save_file_button->text
+			);
+
+			Scene *save_file_scene = create_save_file_scene(save_file_name);
+
+			int scene_i =
+				add_scene_manager(menu_data->manager, save_file_scene);
+
+			select_scene_manager(
+				menu_data->manager, menu_data->window, scene_i
+			);
+		}
+
+		menu_data->clicked_id = -1;
+	}
 }
 
 void draw_load_menu(void *data, SDL_Renderer *renderer) {
